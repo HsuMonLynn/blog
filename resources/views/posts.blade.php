5d3418b7dc991c16a686112f46d193ff3f23958f
HsuMonLynn <scm.hsumonlynn@gmail.com>
2021-10-28 21:09:02 +0630

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post"> My First Posts</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, eum at earum fuga in a sequi dolorum minima amet corrupti harum numquam. Maiores tenetur consequatur magni, reprehenderit quia facilis eligendi!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Posts</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, eum at earum fuga in a sequi dolorum minima amet corrupti harum numquam. Maiores tenetur consequatur magni, reprehenderit quia facilis eligendi!</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Posts</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, eum at earum fuga in a sequi dolorum minima amet corrupti harum numquam. Maiores tenetur consequatur magni, reprehenderit quia facilis eligendi!</p>
+    </article>
+</body>
+</html>
