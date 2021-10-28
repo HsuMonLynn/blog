50f47003a587c1dd553c8bb67fdc811b2005a7a0
HsuMonLynn <scm.hsumonlynn@gmail.com>
2021-10-28 23:25:56 +0630

    Section 2, Ep 10 | Use Caching for expensive operations

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,19 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug) {
 
-    $post = file_get_contents(__DIR__."/../resources/posts/{$slug}.html");
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+
     
+    if(!file_exists($path)){
+
+        return redirect('/');
+    }
+
+    $post = cache()->remember("posts.{slug}", 1200 , fn()=>file_get_contents($path));
+
+    // $post = file_get_contents($path);
+
     return view ('post',[
         'post' => $post
     ]);
-});
+})->where('post','[A-z_\-]+');
