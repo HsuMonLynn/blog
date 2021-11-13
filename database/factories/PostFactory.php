23b0e8722af3003550497f03cc5d733ff7ec1aff
HsuMonLynn <scm.hsumonlynn@gmail.com>
2021-11-13 22:38:57 +0630

    Section 4,Ep-28 | Turbo Boost With Factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{   
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'excerpt' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'user_id' => User::factory(),
+            'category_id' => Category::factory()
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,54 +14,13 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+    {   
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium blandit tellus eget condimentum. Nunc varius, ex sed dapibus pulvinar, massa tellus congue mi, eget vestibulum massa nulla in diam. Sed ac ultricies purus. Aliquam non ipsum non turpis tempus vulputate eget ac sapien. Etiam ut sapien elit. Mauris tempus, quam eu viverra pharetra, eros mauris ultrices erat, id rutrum ligula quam quis tortor. Cras eget commodo odio. Donec est turpis, malesuada a imperdiet ut, tempor ut nunc. Maecenas in sapien blandit, efficitur orci eget, tincidunt dolor. Mauris consequat non augue in consectetur.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium blandit tellus eget condimentum. Nunc varius, ex sed dapibus pulvinar, massa tellus congue mi, eget vestibulum massa nulla in diam. Sed ac ultricies purus. Aliquam non ipsum non turpis tempus vulputate eget ac sapien. Etiam ut sapien elit. Mauris tempus, quam eu viverra pharetra, eros mauris ultrices erat, id rutrum ligula quam quis tortor. Cras eget commodo odio. Donec est turpis, malesuada a imperdiet ut, tempor ut nunc. Maecenas in sapien blandit, efficitur orci eget, tincidunt dolor. Mauris consequat non augue in consectetur.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium blandit tellus eget condimentum. Nunc varius, ex sed dapibus pulvinar, massa tellus congue mi, eget vestibulum massa nulla in diam. Sed ac ultricies purus. Aliquam non ipsum non turpis tempus vulputate eget ac sapien. Etiam ut sapien elit. Mauris tempus, quam eu viverra pharetra, eros mauris ultrices erat, id rutrum ligula quam quis tortor. Cras eget commodo odio. Donec est turpis, malesuada a imperdiet ut, tempor ut nunc. Maecenas in sapien blandit, efficitur orci eget, tincidunt dolor. Mauris consequat non augue in consectetur.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
