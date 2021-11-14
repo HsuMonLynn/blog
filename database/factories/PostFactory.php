0755902cbc099ae7d876a7f588c722b2dac09e06
HsuMonLynn <scm.hsumonlynn@gmail.com>
2021-11-14 21:36:29 +0630

    Section-4, Ep-30 Eager Load  Relationships on an Existing Model

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,10 +18,10 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->sentence,
-            'slug' => $this->faker->slug,
-            'excerpt' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'excerpt' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
             'user_id' => User::factory(),
             'category_id' => Category::factory()
 
